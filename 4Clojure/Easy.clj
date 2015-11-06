4992147f0901a46b7c568e2d25326b722e570f97
Yukun Guo <gyk.net@gmail.com>
2015-11-06 10:29:15 +0800

    Update

--- 4Clojure/Easy.clj
+++ 4Clojure/Easy.clj
@@ -80,3 +80,66 @@ mapcat vector
 
 ;#49
 (juxt take drop)
+
+;#51
+[1 2 3 4 5]
+
+;#61
+#(into {} (map vector %1 %2))
+
+;#62
+(fn iter [f i]
+  (cons i (lazy-seq (iter f (f i)))))
+
+;#63
+(fn [f xs] (reduce #(update-in % [(f %2)] (fnil conj []) %2) {} xs))
+
+;#66
+(fn [a b]
+  (if (= 0 b) a (recur b (rem a b))))
+
+;#81
+(comp set filter)
+
+;#83
+(fn [& bs]
+  (every? true? [(some true? bs) (some false? bs)]))
+
+;#88
+#(set (into (remove %1 %2) (remove %2 %1)))
+
+;#90
+#(set (for [x %1 y %2] [x y]))
+
+;#95
+(fn tree? [t]
+  (or (nil? t)
+      (and (coll? t)
+           (= (count t) 3)
+           (let [[root l-child r-child] t]
+                (and (not (coll? root))
+                     (tree? l-child)
+                     (tree? r-child))))))
+
+;#96
+(fn [[_ l r]]
+  ((fn m? [[l_ ll lr] [r_ rl rr]]
+    (if (= nil l_ r_)
+      true
+      (and (= l_ r_)
+           (m? ll rr)
+           (m? lr rl))))
+    l r))
+
+;#97
+(fn [i]
+  (nth (iterate #(mapv + (conj % 0) (cons 0 %)) [1]) (dec i)))
+
+;#99
+#(->> (* %1 %2) str seq (map (comp read-string str)))
+
+;#100
+(fn [& xs]
+  (let [gcd #(if (= 0 %2) %1 (recur %2 (mod %1 %2)))
+        lcm #(/ (* %1 %2) (gcd %1 %2))]
+    (reduce lcm xs)))
